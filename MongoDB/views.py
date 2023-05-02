a53082fb968db93c1f6ce1ade8041ecf4a2f679a
alf767443 <andreluisfranca98@gmail.com>
2023-05-02 11:09:50 +0100

    Add options method

--- MongoDB/views.py
+++ MongoDB/views.py
@@ -160,10 +160,13 @@ def robot(request, query=''):
     elif request.method == 'OPTIONS':
         try:
             raw=JSONParser().parse(request)
+            print(raw)
             filter = {
                 'robot': raw['robot']
             }
+            print(filter)
             result = MDBchart.find_one(filter=filter, projection={'robot': 1, 'password': 1})
+            print(result)
             if raw['robot'] == result['robot'] and raw['password'] == result['password']:
                 return JsonResponse(data={'_id': result['_id']},safe=False, status=status.HTTP_202_ACCEPTED)
             else:
