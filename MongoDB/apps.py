4308a033d6240d34c99965e38ae5a8ea6c8a57d2
alf767443 <andreluisfranca98@gmail.com>
2023-05-26 20:48:40 +0100

    *

--- MongoDB/apps.py
+++ MongoDB/apps.py
@@ -8,13 +8,9 @@ from .task import execute_background_task
 class MongodbConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'MongoDB'
-
-class BackgroundTaskConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'BackgroundTask'
-
+    
     def ready(self):
-        # Crie uma thread e inicie-a
-        thread = threading.Thread(target=execute_background_task)
-        thread.daemon = True
-        thread.start()
+            # Create a thread and start it
+            thread = threading.Thread(target=execute_background_task)
+            thread.daemon = True
+            thread.start()
